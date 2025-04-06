723092e0b67fc95feca5a197c47f778526ff6d04
cartikeya <cartikeya.official@gmail.com>
2025-04-06 12:26:11 +0530

    designed url scrapping page and added slider functionality

--- URLscraping/urlscraping.js
+++ URLscraping/urlscraping.js
@@ -1,1 +1,12 @@
+const slider = document.getElementById("volumeSlider");
+const output = document.getElementById("volumeValue");
 
+slider.addEventListener("input", function() {
+    output.textContent = slider.value;
+    
+    // Calculate the fill percentage
+    let value = ((slider.value - slider.min) / (slider.max - slider.min)) * 100;
+
+    // Apply dynamic background
+    slider.style.background = `linear-gradient(to right,rgb(0, 0, 0) 0%,rgb(71, 73, 71) ${value}%, #d9d9d9 ${value}%)`;
+});
